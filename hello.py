82e77ce87159e7984b805ee65e11099a78b73d72
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-11-07 23:35:01 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 코딩 시작
+#old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
+
+trs = soup.select('#old_content > table > tbody > tr')
+
+for tr in trs:
+    a_tag = tr.select_one('td.title > div > a')
+    if a_tag is not None:
+        rank = tr.select_one('td:nth-child(1) > img')['alt']
+        title = a_tag.text
+        score = tr.select_one('td.point').text
+        print(rank, title, score)
+
+
